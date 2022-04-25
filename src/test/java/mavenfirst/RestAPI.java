184594bbc1c4da9109b4d8faf3b8110e3a365432
geetha1910 <geethanmurthy19@gmail.com>
2022-04-25 20:05:22 +0530

    first commit

---
+++ src/test/java/mavenfirst/RestAPI.java
@@ -0,0 +1,18 @@
+package mavenfirst;
+
+import org.testng.annotations.Test;
+
+public class RestAPI {
+	@Test
+	public void restAPITest1()
+	{
+		System.out.println("restAPITest1");
+	}
+	@Test
+	public void restAPITest2()
+	{
+		System.out.println("restAPITest2");
+	}
+
+
+}
---
+++ src/test/java/mavenfirst/SeleniumTest.java
@@ -0,0 +1,17 @@
+package mavenfirst;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void seleniumTest1()
+	{
+		System.out.println("seleniumTest1");
+	}
+	@Test
+	public void seleniumTest2()
+	{
+		System.out.println("seleniumTest2");
+	}
+
+}
